d90cd71b08ab42a61a6d623911cc911134ec827f
Jessica B. Kazembe <jbrazelt@thoughtworks.com>
2016-07-07 14:47:51 -0500

    Added json file to populate data on page.

--- scripts/home.controller.js
+++ scripts/home.controller.js
@@ -2,42 +2,16 @@ angular
   .module('lynndupree06')
   .controller('HomeController', HomeController);
 
-function HomeController() {
+HomeController.$inject = [
+  '$http'
+];
+
+function HomeController($http) {
   const vm = this;
 
   vm.types = [];
-  vm.skills = [
-    {'skill': 'C++', 'type': 'language', 'value': 60},
-    {'skill': 'C', 'type': 'language', 'value': 40},
-    {'skill': 'Java', 'type': 'language', 'value': 85},
-    {'skill': 'C#', 'type': 'language', 'value': 60},
-    {'skill': 'PHP', 'type': 'language', 'value': 60},
-    {'skill': 'Ruby', 'type': 'language', 'value': 50},
-    {'skill': 'HTML', 'type': 'web', 'value': 100},
-    {'skill': 'CSS', 'type': 'web', 'value': 100},
-    {'skill': 'Javascript', 'type': 'web', 'value': 80},
-    {'skill': 'JQuery', 'type': 'web', 'value': 70},
-    {'skill': 'Ajax', 'type': 'web', 'value': 60},
-    {'skill': 'ASP.NET MVC', 'type': 'web', 'value': 50},
-    {'skill': 'Spring MVC', 'type': 'web', 'value': 60},
-    {'skill': 'Spring Boot', 'type': 'web', 'value': 60},
-    {'skill': 'Hibernate', 'type': 'framework', 'value': 60},
-    {'skill': 'iBATIS', 'type': 'framework', 'value': 60},
-    {'skill': 'Google Maps API', 'type': 'framework', 'value': 50},
-    {'skill': 'Sencha', 'type': 'framework', 'value': 50},
-    {'skill': 'Backbone', 'type': 'framework', 'value': 50},
-    {'skill': 'AngularJS', 'type': 'framework', 'value': 60},
-    {'skill': 'Rails', 'type': 'framework', 'value': 70},
-    {'skill': 'Windows', 'type': 'system', 'value': 100},
-    {'skill': 'MAC', 'type': 'system', 'value': 100},
-    {'skill': 'Linux', 'type': 'system', 'value': 80},
-    {'skill': 'Unix', 'type': 'system', 'value': 80},
-    {'skill': 'Android', 'type': 'system', 'value': 80},
-    {'skill': 'Eclipse', 'type': 'tool', 'value': 100},
-    {'skill': 'IntelliJ', 'type': 'tool', 'value': 100},
-    {'skill': 'Xamarin', 'type': 'tool', 'value': 90},
-    {'skill': 'Visual Studio', 'type': 'tool', 'value': 80}
-  ];
+  vm.skills = $http.get('content/skills.json');
+  vm.experiences = $http.get('content/experiences.json');
 
   vm.filter = (type) => {
     if(_.contains(vm.types, type)) {
