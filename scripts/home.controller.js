2aac9ad9d118d756466ca4b2b4098e551538a5cf
Jessica B. Kazembe <jbrazelt@thoughtworks.com>
2016-07-08 15:44:43 -0500

    Added year and months into timeline, and hid other experiences when one experience is being viewed.

--- scripts/home.controller.js
+++ scripts/home.controller.js
@@ -9,7 +9,12 @@ HomeController.$inject = [
 function HomeController($http) {
   const vm = this;
 
+  vm.selectedExperience = null;
   vm.types = [];
+  vm.yearList = [
+    2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007
+  ];
+
   $http.get('content/skills.json').success((response) => vm.skills = response);
   $http.get('content/experiences.json').success((response) => vm.experiences = response);
 
@@ -23,5 +28,30 @@ function HomeController($http) {
 
   vm.isSelected = (type) => {
     return vm.types.indexOf(type) > -1;
-  }
+  };
+
+  vm.getDate = (date) => {
+    return moment(new Date(date)).format('MMM');
+  };
+
+  vm.getPosition = (date) => {
+    const yearIntervalHeight = 70;
+    const currentDate = moment(new Date(date));
+    const year = currentDate.year();
+    const yearIdx = vm.yearList.indexOf(year);
+    const spacingInterval = yearIntervalHeight / 12;
+    const bottomPostion = yearIdx * yearIntervalHeight;
+    return bottomPostion - (spacingInterval * currentDate.month());
+  };
+
+  vm.toggleSelectedExperience = (experience) => {
+    if(vm.selectedExperience === experience) {
+      vm.selectedExperience = null;
+    } else {
+      vm.selectedExperience = experience;
+    }
+  };
+
+  vm.shouldShowExperience = (experience) => vm.selectedExperience === experience || vm.selectedExperience === null;
+  vm.shouldShowContent = (experience) => vm.selectedExperience === experience;
 }
